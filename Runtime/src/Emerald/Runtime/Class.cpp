b7a5f892808dc928d38d3b02194247cf4617d4c7
njchensl <nanjingchj@gmail.com>
2020-07-25 09:21:04 -0400

    class header reading finished, untested

--- Runtime/src/Emerald/Runtime/Class.cpp
+++ Runtime/src/Emerald/Runtime/Class.cpp
@@ -1,4 +1,5 @@
 #include "Class.h"
+#include "Utils.h"
 
 namespace Emerald
 {
@@ -57,5 +58,118 @@ namespace Emerald
             break;
         }
         }
+    }
+
+    ClassData ClassFileToClassData(void* file)
+    {
+        const auto HeadRoundUp = [](Byte*& head) -> void
+        {
+            head = reinterpret_cast<Byte*>(RoundUp<ULong, 8>(reinterpret_cast<ULong>(head)));
+        };
+
+
+        Byte* head = static_cast<Byte*>(file);
+
+        ClassData classData;
+
+        //////////////////////////
+        // CLASS HEADER //////////
+        //////////////////////////
+        const auto length = ptr_copy_cast<Byte>(head, sizeof(Byte));
+        head++;
+        classData.Name = reinterpret_cast<const char*>(head);
+        head += static_cast<ULong>(length) + 1ULL;
+        classData.Type = ptr_copy_cast<ClassType>(head, sizeof(ClassType));
+        head++;
+        HeadRoundUp(head);
+        uint numSuperclasses = static_cast<uint>(ptr_copy_cast<Byte>(head, sizeof(Byte)));
+        head++;
+
+        for (uint i = 0; i < numSuperclasses; i++)
+        {
+            const auto lengthSuperclassName = static_cast<size_t>(*head);
+            head++;
+            classData.NameSuperClasses.emplace_back(reinterpret_cast<const char*>(head));
+            head += lengthSuperclassName + 1ULL;
+        }
+
+        HeadRoundUp(head);
+        // constant pool
+        const auto numConstants = ptr_copy_cast<Int>(head, sizeof(Int));
+        HeadRoundUp(head);
+        // constant pool entries
+        for (Int i = 0; i < numConstants; i++)
+        {
+            AChar type = *reinterpret_cast<AChar*>(head);
+            head++;
+            std::any data;
+            switch (type)
+            {
+            case 'A': case 's':
+            {
+                size_t stringLength = static_cast<size_t>(*head);
+                head++;
+                String stringConstant = reinterpret_cast<const char*>(head);
+                data = stringConstant;
+                head += stringLength + 1;
+                break;
+            }
+            case 'B':
+            {
+                data = ptr_copy_cast<Byte>(head, sizeof(Byte));
+                head++;
+                break;
+            }
+            case 'S':
+            {
+                data = ptr_copy_cast<Short>(head, sizeof(Short));
+                head += sizeof(Short);
+                break;
+            }
+            case 'I':
+            {
+                data = ptr_copy_cast<Int>(head, sizeof(Int));
+                head += sizeof(Int);
+                break;
+            }
+            case 'J':
+            {
+                data = ptr_copy_cast<Long>(head, sizeof(Long));
+                head += sizeof(Long);
+                break;
+            }
+            case 'F':
+            {
+                data = ptr_copy_cast<Float>(head, sizeof(Float));
+                head += sizeof(Float);
+                break;
+            }
+            case 'D':
+            {
+                data = ptr_copy_cast<Double>(head, sizeof(Double));
+                head += sizeof(Double);
+                break;
+            }
+            case 'C':
+            {
+                data = ptr_copy_cast<Char>(head, sizeof(Char));
+                head += sizeof(Char);
+                break;
+            }
+            case 'Z':
+            {
+                data = ptr_copy_cast<Boolean>(head, sizeof(Boolean));
+                head += sizeof(Boolean);
+                break;
+            }
+            }
+            classData.ConstantPool.emplace_back(type, data);
+            HeadRoundUp(head);
+        }
+
+        //////////////////////////////////
+        // CLASS DATA ////////////////////
+        //////////////////////////////////
+        return classData;
     }
 }
--- Runtime/src/Emerald/Runtime/Class.h
+++ Runtime/src/Emerald/Runtime/Class.h
@@ -9,6 +9,14 @@ namespace Emerald
 {
 #define ALIGN8 alignas(8)
 
+    enum class ClassType : unsigned char
+    {
+        Class = 0,
+        Interface = 1
+    };
+
+    static_assert(sizeof(ClassType) == 1);
+
     union ALIGN8 PrimitiveData
     {
         Byte ByteValue;
@@ -27,13 +35,101 @@ namespace Emerald
     {
         ConstantPoolEntry(AChar type, std::any data);
 
-        ConstantPoolEntry(const ConstantPoolEntry& other) = delete;
+        ConstantPoolEntry(const ConstantPoolEntry& other) : Type(other.Type)
+        {
+            switch (other.Type)
+            {
+            case 'A':
+            {
+                ClassName = other.ClassName;
+                break;
+            }
+            case 's':
+            {
+                StringConstant = other.StringConstant;
+                break;
+            }
+            default:
+            {
+                memcpy(&PrimitiveValue, &other.PrimitiveValue, sizeof(PrimitiveData));
+                break;
+            }
+            }
+        }
 
-        ConstantPoolEntry(ConstantPoolEntry&& other) noexcept = delete;
+        ConstantPoolEntry(ConstantPoolEntry&& other) noexcept : Type(other.Type)
+        {
+            switch (other.Type)
+            {
+            case 'A':
+            {
+                ClassName = std::move(other.ClassName);
+                break;
+            }
+            case 's':
+            {
+                StringConstant = std::move(other.StringConstant);
+                break;
+            }
+            default:
+            {
+                memcpy(&PrimitiveValue, &other.PrimitiveValue, sizeof(PrimitiveData));
+                break;
+            }
+            }
+        }
 
-        ConstantPoolEntry& operator=(const ConstantPoolEntry& other) = delete;
+        ConstantPoolEntry& operator=(const ConstantPoolEntry& other)
+        {
+            if (this == &other)
+            {
+                return *this;
+            }
+            switch (other.Type) {
+            case 'A':
+            {
+                ClassName = other.ClassName;
+                break;
+            }
+            case 's':
+            {
+                StringConstant = other.StringConstant;
+                break;
+            }
+            default:
+            {
+                memcpy(&PrimitiveValue, &other.PrimitiveValue, sizeof(PrimitiveData));
+                break;
+            }
+            }
+            return *this;
+        }
 
-        ConstantPoolEntry& operator=(ConstantPoolEntry&& other) noexcept = delete;
+        ConstantPoolEntry& operator=(ConstantPoolEntry&& other)
+        {
+            if (this == &other)
+            {
+                return *this;
+            }
+            switch (other.Type) {
+            case 'A':
+            {
+                ClassName = std::move(other.ClassName);
+                break;
+            }
+            case 's':
+            {
+                StringConstant = std::move(other.StringConstant);
+                break;
+            }
+            default:
+            {
+                memcpy(&PrimitiveValue, &other.PrimitiveValue, sizeof(PrimitiveData));
+                break;
+            }
+            }
+            return *this;
+        }
 
         ~ConstantPoolEntry() noexcept
         {
@@ -51,6 +147,7 @@ namespace Emerald
             }
             }
         }
+
         AChar Type; // char code for primitives, ‘A’ for classes, ‘s’ for strings
         union ALIGN8
         {
@@ -85,8 +182,9 @@ namespace Emerald
     struct ALIGN8 ClassData
     {
         String Name;
-        AChar Type;
-        Int NumberOfConstants;
+        ClassType Type;
+        std::vector<String> NameSuperClasses;
+        std::vector<ConstantPoolEntry> ConstantPool;
     };
 
     // linked classes and code
@@ -102,9 +200,12 @@ namespace Emerald
         MethodData m_MethodData;
     };
 
+    // class data can be copied, but class data can't
     class Class
     {
         ClassData m_ClassData;
     };
+
+    ClassData ClassFileToClassData(void* file);
 #undef ALIGN8
 }
